be43a703ec2f897be10e65f126a617caa596af15
lam.nguyen <lam.nguyen@metanet.co.kr>
2023-07-27 16:46:49 +0700

    update code

---
+++ components/Heros.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Heros = () => {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">Title</h1>
+        <p className="hero__subTitle">subTitle</p>
+      </div>
+    </div>
+  );
+};
+
+export default Heros;
